babbaeab194fe6ea2518a19aa6d0d4bb05790d93
sweekriti121 <89648904+sweekriti121@users.noreply.github.com>
2026-05-04 22:46:32 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            int diff=target-nums[i];
+            if(m.count(diff))
+            {
+                if(i<m[diff])
+                    return {i,m[diff]};
+                else
+                    return {m[diff],i};
+            }
+            m[nums[i]]=i;
+        }
+    }
+};
